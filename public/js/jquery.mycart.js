b06d0b612249d5de68c46c5edd52cc5430833a39
Simms <Sabrina.Simms@us.gt.com>
2017-11-29 20:01:00 -0500

    shopping cart added

--- public/js/jquery.mycart.js
+++ public/js/jquery.mycart.js
@@ -6,6 +6,7 @@
 
 (function ($) {
 
+
   "use strict";
 
   var OptionManager = (function () {
@@ -381,3 +382,43 @@
 
 
 })(jQuery);
+
+
+$(function () {
+
+  var goToCartIcon = function($addTocartBtn){
+    var $cartIcon = $(".my-cart-icon");
+    var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+    $addTocartBtn.prepend($image);
+    var position = $cartIcon.position();
+    $image.animate({
+      top: position.top,
+      left: position.left
+    }, 500 , "linear", function() {
+      $image.remove();
+    });
+  }
+
+  $('.my-cart-btn').myCart({
+    classCartIcon: 'my-cart-icon',
+    classCartBadge: 'my-cart-badge',
+    affixCartIcon: true,
+    checkoutCart: function(products) {
+      $.each(products, function(){
+        console.log(this);
+      });
+    },
+    clickOnAddToCart: function($addTocart){
+      goToCartIcon($addTocart);
+    },
+    getDiscountPrice: function(products) {
+      var total = 0;
+      $.each(products, function(){
+        total += this.quantity * this.price;
+      });
+      return total * 0.5;
+    }
+  });
+
+});
+

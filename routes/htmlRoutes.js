fbb8fb2eaaaed87bab4fe5a79baee9805e5651e2
John Yencha III <mikeydoughnuts@johns-mbp.lan>
2017-12-08 11:53:18 -0500

    local

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,25 +4,28 @@
 
 // Dependencies
 // =============================================================
-var path = require(“path”);
+var path = require('path');
 // Requiring our models
 // var db = require(“../models”);
 
 // Routes
 // =============================================================
 module.exports = function(app) {
- app.get(“/all”, function(req, res){
-   res.sendFile(path.join(__dirname, “../public/index.html”));
+ app.get('/all', function(req, res){
+   res.sendFile(path.join(__dirname, '../public/index.html'));
  });
 
- app.get(“/packages”, function(req, res){
-   res.sendFile(path.join(__dirname, “../public/packages.html”));
+ app.get('/packages', function(req, res){
+   res.sendFile(path.join(__dirname, '../public/packages.html'));
  });
 
- app.get(“/ourStory”, function(req, res){
-   res.sendFile(path.join(__dirname, “../public/our-story.html”))
+ app.get('/ourStory', function(req, res){
+   res.sendFile(path.join(__dirname, '../public/our-story.html'))
  });
 
+ app.get('/home', function(req, res){
+  res.sendFile(path.join(__dirname, '../public/home.html'))
+});
  // app.get(“/contact”, function (req, res){
  //   res.sendFile(path.join(__dirname, “../public/contact.html”))
  // });
